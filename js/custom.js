a60659cfa777df6f0cb494f41351b83990cf5a04
DESK_ADMIN <DESK_ADMIN@prutech.local>
2022-09-27 17:21:27 +0530

    Making changes in data layer

--- js/custom.js
+++ js/custom.js
@@ -11,3 +11,21 @@ function openNav() {
     document.getElementById("myNav").classList.toggle("menu_width")
     document.querySelector(".custom_menu-btn").classList.toggle("menu_btn-style")
 }
+
+
+// test js 1
+// var isLoggedIn = user.getLocal('authStatus')|| '';
+
+// if(isLoggedIn === 'true' || mbox.param('Authenticated') === 'true'){
+//   return 'user Logged In';
+// } else{
+//   return 'user not Logged In';
+// }
+
+
+
+// // test js 2
+
+// user.setLocal('authStatus','');
+// var page = mbox.param('Authenticated');
+// return page;
